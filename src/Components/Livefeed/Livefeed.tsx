453f0d951ceec400f063ae966b0acc8a4a3000f9
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-05-31 13:56:57 -0400

    updates to camera

--- src/Components/Livefeed/Livefeed.tsx
+++ src/Components/Livefeed/Livefeed.tsx
@@ -1,7 +1,9 @@
 import React, { useRef, useEffect } from "react";
+import { config } from '../config'
 
 export default function LiveFeed() {
     const videoRef = useRef<HTMLVideoElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
       if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -16,6 +18,38 @@ export default function LiveFeed() {
             }
             video.srcObject = stream;
             video.play();
+
+            const videoInterval = setInterval(() => {
+              const video = videoRef.current
+              const canvas = canvasRef.current
+              if (canvas === null) {
+                return
+              }
+              const context = canvas.getContext('2d')
+              if (video === null || context === null) {
+                return
+              }
+              canvas.width = video.videoWidth;
+              canvas.height = video.videoHeight;
+              context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+              const imageData = canvas.toDataURL('image/jpeg');
+              
+              fetch(`${config.backendUrl}/imagescan`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ image: imageData }),
+              })
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+              });
+              
+            }, 5000)
+
+            return () => {clearInterval(videoInterval)}
+
           })
           .catch((err) => {
             console.error("error:", err);
@@ -23,5 +57,10 @@ export default function LiveFeed() {
       }
     }, []);
   
-    return <video style={{width: "100%"}} ref={videoRef} autoPlay muted playsInline/>;
+    return (
+      <>
+        <video style={{width: "100%"}} ref={videoRef} autoPlay muted playsInline/>
+        <canvas ref={canvasRef} style={{ display: 'none' }}/>
+      </>
+    )
 }
